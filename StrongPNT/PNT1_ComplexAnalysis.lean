1656aca0e78d6be841313abb303ee5890925d07e
sebastian-griego <sebastianngriego@gmail.com>
2025-09-23 02:26:10 +0000

    Auto-commit at 2025-09-23T02:16:26Z - 164 sorries

--- StrongPNT/PNT1_ComplexAnalysis.lean
+++ StrongPNT/PNT1_ComplexAnalysis.lean
@@ -534,7 +534,8 @@ lemma lem_analAtOnOn (R : Real) (h : Complex → Complex) (_hR : 0 < R)
     -- Since {z | norm z ≤ R ∧ z ≠ 0} ⊆ {z | norm z ≤ R}, we can use monotonicity
     apply AnalyticWithinAt.mono h'
     -- Show {z | norm z ≤ R ∧ z ≠ 0} ⊆ {z | norm z ≤ R}
-    sorry
+    intro w ⟨hw_norm, _⟩
+    exact hw_norm
 
 def ballDR (R : Real) : Set Complex := {z : Complex | norm z < R}
 
